509c3899581f3778b9a2c29794d904f64bb4da7f
tarunrk <tarun.vamos@gmail.com>
2016-10-13 18:24:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,9 @@
 console.log('Loaded!');
+//change the content to new value
+var element=document.getElementById('main-text');
+element.innerHTML="New Value";
+
+var img=document.getElementById('madi');
+madi.onclick=function () {
+    madi.style.marginLeft='100px';
+}
